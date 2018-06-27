a3d8c8b9a2e69933ec4609cd63357503b830a958
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-06-27 09:37:18 +0200

    Galser

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+
+var closeNav = true;
 $(document).ready(function(){
     $('#nav-icon').click(function(){
         $(this).toggleClass('open');
+        if (closeNav === true){
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        closeNav = false;
+        }
+        else {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft= "0";
+            document.body.style.backgroundColor = "white";
+            closeNav = true;
+        }
     });
 });
